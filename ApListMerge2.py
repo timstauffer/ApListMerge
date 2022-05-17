de41f1cefe5f4b6331eabdfe66999f07d750eb7b
Tim Stauffer <wonk@timstauffer.me>
2022-05-17 19:49:24 -0400

    Update ApListMerge2.py
    Updates as I teach coworker about Python and our AP list import process

--- ApListMerge2.py
+++ ApListMerge2.py
@@ -1,7 +1,7 @@
 import os
-os.chdir('c:\\Users\\TS00017847\\desktop\\python')
+os.chdir('c:\\AP_Mapping')
 
-with open("AP.csv","r") as s, open("AccessPointList.csv", 'r') as t, open("Output.csv", 'w') as o:
+with open("IT_List.csv","r") as s, open("MedNet_List.csv", 'r') as t, open("Output.csv", 'w') as o:
 
     # Read in IT list and MedNet export file
     lines = s.readlines()
@@ -14,13 +14,17 @@ with open("AP.csv","r") as s, open("AccessPointList.csv", 'r') as t, open("Outpu
     for i in range(0, len(lines)):  # Loop through IT list
         line = lines[i]
         key = line[:16]            # First 11 characters of MAC
-        desc = line[18:]            # Description to be used in output file
 
+        if len(line) < 60:
+            desc = line[18:-1]            # Description to be used in output file.
+        else:                             # 40 char max. Strip off CR/LF.
+            desc = line[18:58]
+        
         for j in range(0, len(records)):        # Loop through MedNet export
             record = records[j]
             lock = record[:16]
             locker = record[:17]
-          
+        
             if key.lower() == lock.lower():     # both to lower case and compare first 11 char
                 newline = locker + "," + desc   # Generate record using MAC from MedNet export and description from IT list
-                o.write(newline)                # Write a record to the new import file
+                o.write(newline + "\n")              # Write a record to the new import file
